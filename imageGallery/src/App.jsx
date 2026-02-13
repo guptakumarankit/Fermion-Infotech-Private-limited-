51524edc253ca1a472f93c0c4c4b539146cf9c93
guptakumarankit <ankit.kr778389@gmail.com>
2026-02-13 18:55:23 +0530

    update

--- imageGallery/src/App.jsx
+++ imageGallery/src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="h-screen w-screen flex flex-col  items-center gap-3 bg-blue-100">
+    <div className="h-screen w-screen flex flex-col  items-center gap-3 bg-blue-100 overflow-hidden">
       <BrowserRouter>
         <NavBar />
         <MainRoute />
--- imageGallery/src/main.jsx
+++ imageGallery/src/main.jsx
@@ -9,6 +9,5 @@ createRoot(document.getElementById("root")).render(
     <ContextProvider>
       <App />
     </ContextProvider>
-    ,
   </StrictMode>,
 );
